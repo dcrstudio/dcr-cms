8387f53c3fe91e4d50ccdff9831cba7819a0c8c5
Antony J. Duran Hernandez <ajdurancr@gmail.com>
2021-05-05 17:32:02 -0600

    remove graphql root typeDefs

--- packages/server/graphql/index.js
+++ packages/server/graphql/index.js
@@ -2,15 +2,13 @@ import { makeExecutableSchema } from 'graphql-tools'
 import { mergeTypeDefs, mergeResolvers } from '@graphql-toolkit/schema-merging'
 import { createSchemaDefinition } from 'safis-cms-schema'
 
-import rootTypeDefs from './typeDefs'
-
 const createSchema = (contentTypes, schemaUpdater) => {
   const {
     typeDefs: cmsTypeDefs,
     resolvers: cmsResolvers,
   } = createSchemaDefinition(contentTypes, schemaUpdater)
 
-  const typeDefsList = [rootTypeDefs, ...cmsTypeDefs]
+  const typeDefsList = [...cmsTypeDefs]
   const resolversList = [...cmsResolvers]
 
   const typeDefs = mergeTypeDefs(typeDefsList, { all: true })
